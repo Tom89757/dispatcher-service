90608c92b8b7ee558c82f9c60ac410897c17801f
Tom89757 <2689980244@qq.com>
2023-08-31 21:36:02 +0800

    init

---
+++ src/test/java/com/polarshop/dispatcherservice/DispatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarshop.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
